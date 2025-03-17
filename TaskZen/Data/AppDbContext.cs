d8dd6fa8111ca122d2555294e0d0434cfaea3b50
medi77na <medinastevenurregodavid@gmail.com>
2025-03-17 07:17:17 -0500

    Se realiza el modelo y la migración correspondiente.

--- TaskZen/Data/AppDbContext.cs
+++ TaskZen/Data/AppDbContext.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using TaskZen.Models.User;
 
 namespace TaskZen.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+            DbSet<UserModel> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            optionsBuilder.ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
         }
     }
-    
 }
---
+++ TaskZen/Models/User/UserModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskZen.Models.User
+{
+    [Table("User")]
+    public class UserModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("last_name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Column("date_birth")]
+        public DateOnly DateBirth { get; set; }
+
+        [Required]
+        [Column("password")]
+        public string Password { get; set; }
+    }
+}
