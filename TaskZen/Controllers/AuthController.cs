583a8690f4ed9c0e2a99b7fd11e8590c94f3109f
medi77na <medinastevenurregodavid@gmail.com>
2025-03-18 18:17:25 -0500

    Se realizan los formularios de autenticación de forma correcta

---
+++ TaskZen/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskZen.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
